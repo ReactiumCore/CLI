4ed5dd6d9b11a4a79c463dc7a056e30b9ea0da27
Cam Tullos <cam@tullos.ninja>
2020-05-18 15:10:16 -0400

    Added api commands to project command.

---
+++ commands/project/init/actions/api.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const fs = require('fs-extra');
+const chalk = require('chalk');
+const _ = require('underscore');
+const op = require('object-path');
+const ActionSequence = require('action-sequence');
+
+const { arcli, Hook, Spinner } = global;
+const mod = path.dirname(require.main.filename);
+
+module.exports = () => {
+    let params;
+
+    const props = op.get(arcli, 'props');
+    const cwd = op.get(arcli, 'props.cwd');
+    const normalize = (...args) => path.normalize(path.join(...args));
+
+    return {
+        init: args => {
+            params = op.get(args, 'params');
+        },
+        directory: () => {
+            Spinner.message('Creating', chalk.cyan('API'), 'directory...');
+
+            const dir = normalize(cwd, 'API');
+
+            fs.ensureDirSync(dir);
+            fs.emptyDirSync(dir);
+        },
+        install: () => {
+            const actions = require(`${mod}/commands/actinium/install/actions`)(Spinner);
+            return ActionSequence({
+                actions,
+                options: {
+                    params,
+                    props: { ...props, cwd: normalize(cwd, 'API') },
+                },
+            });
+        },
+    };
+};
