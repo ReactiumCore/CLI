c0171b1236fc622fd61d1c54f54c2dcf47388ad0
Cam Tullos <cam@reactium.io>
2022-11-16 09:59:06 -1000

    cli command refactored.

--- commands/cli/command/actions.js
+++ commands/cli/command/actions.js
@@ -1,17 +1,18 @@
-const path = require('path');
-const fs = require('fs-extra');
-const chalk = require('chalk');
-const handlebars = require('handlebars').compile;
+import fs from 'fs-extra';
+import chalk from 'chalk';
+import hb from 'handlebars';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
-module.exports = spinner => {
-    const NOW = Date.now();
-
+export default spinner => {
     const message = text => {
         if (!spinner) return;
         spinner.text = text;
     };
 
     const generate = ({ params, templateFile }) => {
+        const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
         // prettier-ignore
         const actionType = params.overwrite === true ? 'overwritting' : 'creating';
 
@@ -21,7 +22,7 @@ module.exports = spinner => {
         // Template content
         // prettier-ignore
         const template = arcli.normalizePath(__dirname, 'template', `${templateFile}.hbs`);
-        const content = handlebars(fs.readFileSync(template, 'utf-8'))(params);
+        const content = hb.compile(fs.readFileSync(template, 'utf-8'))(params);
         const filepath = arcli.normalizePath(params.destination, templateFile);
 
         fs.writeFileSync(filepath, content);
--- commands/cli/command/generator.js
+++ commands/cli/command/generator.js
@@ -1,31 +1,31 @@
+import ora from 'ora';
+import Actions from './actions.js';
+import ActionSequence from 'action-sequence';
 
-const ora = require('ora');
-const ActionSequence = require('action-sequence');
-
-
-const spinner = ora({
-    spinner : 'dots',
-    color   : 'cyan'
-});
-
-const actions = require('./actions')(spinner);
-
-module.exports = ({ params, props }) => {
+export default ({ params, props }) => {
     console.log('');
-    spinner.start();
 
+    const spinner = ora({
+        spinner: 'dots',
+        color: 'cyan',
+    });
+
+    spinner.start();
+    const actions = Actions(spinner);
     return ActionSequence({
         actions,
-        options: { params, props }
-    }).then((success) => {
-        spinner.start();
-        spinner.succeed('Command creation complete!');
-        console.log('');
-        return success;
-    }).catch((error) => {
-        spinner.start();
-        spinner.fail('Command creation error!');
-        console.log(error);
-        return error;
-    });
+        options: { params, props },
+    })
+        .then(success => {
+            spinner.start();
+            spinner.succeed('Command creation complete!');
+            console.log('');
+            return success;
+        })
+        .catch(error => {
+            spinner.start();
+            spinner.fail('Command creation error!');
+            console.log(error);
+            return error;
+        });
 };
